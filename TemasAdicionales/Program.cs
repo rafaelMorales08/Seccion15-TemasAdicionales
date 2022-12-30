ab2d5193a292f62bd80cf9a0d1f00f0c9ab338bb
rmorales <rmorales@MTY-LP-RMORALES>
2022-12-30 17:46:48 -0600

    Agregar archivos de proyecto.

---
+++ TemasAdicionales/Program.cs
@@ -0,0 +1,7 @@
+﻿var httpClient = new HttpClient();
+
+var respuesta = await httpClient.GetAsync("https://localhost:7216/WeatherForecast");
+
+var cuerpo = await respuesta.Content.ReadAsStringAsync();
+
+Console.WriteLine(cuerpo);
